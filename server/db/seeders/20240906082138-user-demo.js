64621140a470e67bf601a637f0503e96330e7fbc
Komrakova <baby-sale@mail.ru>
2024-09-06 14:16:27 +0300

    v2

--- server/db/seeders/20240906082138-user-demo.js
+++ server/db/seeders/20240906082138-user-demo.js
@@ -4,13 +4,29 @@ const bcrypt = require('bcrypt');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Users', [{
+    await queryInterface.bulkInsert('Users', [
+      {
       name: 'John Doe',
       email: 'john@example.com',
       password: await bcrypt.hash('password123', 10), 
       createdAt: new Date(),
       updatedAt: new Date()
-    }], {});
+    },
+    {
+      name: 'Sam',
+      email: 'sam@sam',
+      password: await bcrypt.hash('123', 10),
+      updatedAt: new Date(),
+      createdAt: new Date(),
+    },
+    {
+      name: 'Eva',
+      email: 'eva@eva',
+      password: await bcrypt.hash('123', 10),
+      updatedAt: new Date(),
+      createdAt: new Date(),
+    },
+  ], {});
   },
 
   async down(queryInterface, Sequelize) {
